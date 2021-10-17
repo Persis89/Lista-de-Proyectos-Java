9064300676fe64b38a4c146f025482960187201d
Jonathan R <91564147+Persis89@users.noreply.github.com>
2021-10-17 12:08:35 -0500

    Proyecto mi primer Hola Mundo

---
+++ Hola_Mundo.java
@@ -0,0 +1,12 @@
+package básico; // Paquete	
+
+public class Hola_Mundo { // Clase
+
+	public static void main(String[] args) { // Method Main
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Mundo");
+
+	}
+
+}
